b819c59a0fa6943bedd56c7103b889d675b3601b
doinggreatt <alzhan.m.1111@gmail.com>
2025-08-24 16:51:45 +0500

    Food JSON Schemas

--- src/apps/food_menu/schemas.py
+++ src/apps/food_menu/schemas.py
@@ -1,8 +1,43 @@
+from typing import Any
+
 from pydantic import BaseModel
 
 
+# ========== Food | FoodSize | FoodType ===============
+
+# ================== Food Type ========================
+
+class WriteFoodType(BaseModel):
+    name: str
+
+class ReadFoodType(BaseModel):
+    id: int
+    name: str
+
+# =================== FoodSize =========================
+
+class WriteFoodSizeSchema(BaseModel):
+    name: str
+    is_new: bool | None = False
+    price: float
+
+class ReadFoodSizeSchema(WriteFoodSizeSchema):
+    id: int
+
+# ==================== Food =============================
+
 class WriteFoodSchema(BaseModel):
     food_name: str
-    modifiers: list[int] = []
-    is_new: bool = False
+    food_sizes: list[WriteFoodSizeSchema]
+    is_new: bool
+    possible_food_modifiers: list[int]
 
+# =========== Modifier | Modifier Option =========
+
+class WriteModifierCategorySchema(BaseModel):
+    name: str
+
+class WriteModifierOptionSchema(BaseModel):
+    modifier_category_id: id
+    name: str
+    price: float
