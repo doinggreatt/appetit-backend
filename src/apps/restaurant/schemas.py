78f41f8f44086f1e19838001250ce30a534aa837
doinggreatt <alzhan.m.1111@gmail.com>
2025-08-28 22:38:21 +0500

    Restaurant write schema

--- src/apps/restaurant/schemas.py
+++ src/apps/restaurant/schemas.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel
 
 class WriteSingleRestaurantSchema(BaseModel):
     name: str
+    lat: float
+    lon: float
 
 class ReadSingleRestaurantSchema(WriteSingleRestaurantSchema):
     id: int
